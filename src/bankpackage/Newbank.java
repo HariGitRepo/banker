0914b289e16a512db0b31806bcdf0af907b6314e
HariGitRepo <90850288+HariGitRepo@users.noreply.github.com>
2023-07-16 20:07:14 +0530

    reset

--- src/bankpackage/Newbank.java
+++ src/bankpackage/Newbank.java
@@ -3,8 +3,8 @@ package bankpackage;
 public class Newbank {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("in bank class");
+		  System.out.println("in test class");
+    System.out.println("in bank class");
 	}
 
 }
